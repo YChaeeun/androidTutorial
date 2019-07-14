89cb81c7b331ee96bf176a80a4940cdfe186f6d1
YChaeeun <se_same5315@naver.com>
2019-07-15 04:04:49 +0900

    BMI calculator : create layout

--- app/src/main/java/com/example/practice/ResultActivity.kt
+++ app/src/main/java/com/example/practice/ResultActivity.kt
@@ -2,14 +2,11 @@ package com.example.practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class ResultActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.bmi_calculator)
-
-
+        setContentView(R.layout.activity_result)
     }
 }
